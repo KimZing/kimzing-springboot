285f0db11374ff878fc24d3be68d84b5e2c752e4
kimzing <kimzing@163.com>
2022-07-24 00:01:17 +0800

    添加kafka基本示例

--- springboot-mq/src/main/java/com/kimzing/mq/kafka/basic/BasicConsumer.java
+++ springboot-mq/src/main/java/com/kimzing/mq/kafka/basic/BasicConsumer.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Component;
 
 
 @Component
-public class ConsumerListener {
+public class BasicConsumer {
     @KafkaListener(topics = "basic",groupId = "basic-group")
     public void onMessage(String msg){
         System.out.println("----basic 收到消息："+msg+"----");
